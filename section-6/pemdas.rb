af19b2ddca229840c7ec1aea1552b656a417047f
Tyronne Calvin <tcalvin571@gmail.com>
2018-05-03 11:59:06 -0500

    section-6 done

---
+++ section-6/pemdas.rb
@@ -0,0 +1,19 @@
+#PEMDAS
+
+pemdas = "
+P - Parentheses '()'
+E - Exponents '**'
+M - Multiplication '*'
+D - Division '/'
+A - Addition '+'
+S - Subtraction '-'
+
+"
+
+puts pemdas
+
+# Integer (2, 5, 5673)
+# Float (1.2,1.3, 6.2)
+# Decimal (2.364231, 3.2222)
+
+p 100.00/80.00
